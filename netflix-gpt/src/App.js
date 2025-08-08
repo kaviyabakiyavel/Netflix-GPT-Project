f8f8fcaacd40d364ac566aab01633f490bf54eba
Kaviya Bakiyavel <40729713+kaviyabakiyavel@users.noreply.github.com>
2025-08-08 00:36:33 +0000

    setup project

--- netflix-gpt/src/App.js
+++ netflix-gpt/src/App.js
@@ -1,6 +1,6 @@
 function App() {
   return (
-      <div className="text-3xl font-bold underline text-blue-500">
+      <div className="text-3xl font-bold text-red-800">
         Hello Tailwind!
       </div>
   );
