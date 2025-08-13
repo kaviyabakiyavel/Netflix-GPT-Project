aa5d73f43af3df13df183e92ce70e28efdc58107
Kaviya Bakiyavel <40729713+kaviyabakiyavel@users.noreply.github.com>
2025-08-13 00:28:46 +0000

    corrected body component

--- netflix-gpt/src/components/Body.js
+++ netflix-gpt/src/components/Body.js
@@ -1,28 +1,25 @@
-import React from 'react'
-import Login from './Login'
-import  Browser from './Browser'
-import { createBrowserRouter } from 'react-router-dom'
-import {RouterProvider} from "react-router-dom"
+import React from "react";
+import Login from "./Login";
+import Browser from "./Browser";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/browser",
+    element: <Browser />,
+  },
+]);
 
 const Body = () => {
-  const appRouter = createBrowserRouter([
-        {
-            path:"/",
-            element: <Login/>
-        },
-        {
-            path: "/browser",
-            element: <Browser/>
-        }
-  ]);
-
   return (
     <div>
-      <RouterProvider router={appRouter}/>
-      <Login/>
-      <Browser/>
+      <RouterProvider router={appRouter} />
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
